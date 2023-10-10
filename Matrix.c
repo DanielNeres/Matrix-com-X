a3f8231d7043f9f0bc2ec2a894e5e9f10a523199
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-10-10 18:39:04 -0300

    Matrix2.0.c

--- Matrix.c
+++ Matrix.c
@@ -1,21 +1,35 @@
 #include<stdio.h>
 
 int main(){
-	int i, j, matriz = 0, escolha = 0;
-	printf("\n");
+int i, j, matriz = 0, escolha = 0;
+printf("\n");
 
-	for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			matriz++;
-            printf("\t%d", matriz);
-		}
-		printf("\n");
+		matriz++;
+    printf("\t%d", matriz);
 	}
-	printf("digite o numero que deseja trocar por X\n");
-	scanf("%d", &escolha);
+	printf("\n");
+}
+printf("digite o numero que deseja trocar por X\n");
+scanf("%d", &escolha);
+matriz = 0;
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
+	{
+		matriz++;
+			if (matriz == escolha)
+			{
+				printf("\tX");
+			}else{
+	    printf("\t%d", matriz);
+	}
+	}
+	printf("\n");
+}
 
-	
-    return 0;
+return 0;
 }
